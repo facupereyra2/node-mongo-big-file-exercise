b04d60f97d1ab12f7525f45881cfe65b7e6d7158
Facundo Pereyra <facupereyra3@gmail.com>
2023-02-12 17:55:28 -0300

    Completed

--- src/controller.js
+++ src/controller.js
@@ -1,12 +1,18 @@
 const Records = require('./records.model');
+const csvToJson = require('csvtojson');
 
 const upload = async (req, res) => {
-    const {file} = req;
+    const {file} = req; //Recibimos el archivo enviado por req
+    try{
+        const csv = await csvToJson().fromFile(file.path); // Transformamos el csv a json para poder cargarlo en la bd.   
+        await Records.insertMany(csv, {ordered: false, writeConcern:{w:0}}) /* Insertamos los registros en la bd. 
+                                                                            Seteamos ordered en false y writeConcern en 0 para mejorar el rendimiento*/
+        res.send("Upload completed")
+    } catch (error) {
+        res.send("Upload failed", error)
+    }
+}
 
-    /* Acá va tu código! Recordá que podés acceder al archivo desde la constante file */
-
-    return res.status(200).json({ message: 'some response' });
-};
 
 const list = async (_, res) => {
     try {
